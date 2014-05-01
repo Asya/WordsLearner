5ff970054e65cf09346eebe3cb9673f065658289
Anastasiia Sukhanovska <stacy4313@gmail.com>
2014-05-01 16:15:38 +0200

    Added SwipeListView

---
+++ WordsLearner/src/com/example/WordsLearner/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.WordsLearner;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import com.fortysevendeg.swipelistview.BaseSwipeListViewListener;
+import com.fortysevendeg.swipelistview.SwipeListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private static final int REQUEST_CODE_SETTINGS = 0;
+    private PackageAdapter adapter;
+    private List<PackageItem> data;
+
+    private SwipeListView swipeListView;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.swipe_list_view_activity);
+
+        data = new ArrayList<PackageItem>();
+
+        adapter = new PackageAdapter(this, data);
+
+        swipeListView = (SwipeListView) findViewById(R.id.example_lv_list);
+
+        swipeListView.setSwipeListViewListener(new BaseSwipeListViewListener() {
+            @Override
+            public void onOpened(int position, boolean toRight) {
+            }
+
+            @Override
+            public void onClosed(int position, boolean fromRight) {
+            }
+
+            @Override
+            public void onListChanged() {
+            }
+
+            @Override
+            public void onMove(int position, float x) {
+            }
+
+            @Override
+            public void onStartOpen(int position, int action, boolean right) {
+                Log.d("swipe", String.format("onStartOpen %d - action %d", position, action));
+            }
+
+            @Override
+            public void onStartClose(int position, boolean right) {
+                Log.d("swipe", String.format("onStartClose %d", position));
+            }
+
+            @Override
+            public void onClickFrontView(int position) {
+                Log.d("swipe", String.format("onClickFrontView %d", position));
+            }
+
+            @Override
+            public void onClickBackView(int position) {
+                Log.d("swipe", String.format("onClickBackView %d", position));
+            }
+
+            @Override
+            public void onDismiss(int[] reverseSortedPositions) {
+                for (int position : reverseSortedPositions) {
+                    data.remove(position);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+        });
+
+        swipeListView.setAdapter(adapter);
+
+        reload();
+
+        new ListAppTask().execute();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage(getString(R.string.loading));
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+
+    }
+
+    private void reload() {
+        swipeListView.setSwipeActionLeft(SwipeListView.SWIPE_ACTION_REVEAL);
+        swipeListView.setSwipeOpenOnLongPress(true);
+    }
+
+    public int convertDpToPixel(float dp) {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return (int) px;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_SETTINGS:
+                reload();
+        }
+    }
+
+    public class ListAppTask extends AsyncTask<Void, Void, List<PackageItem>> {
+
+        protected List<PackageItem> doInBackground(Void... args) {
+            PackageManager appInfo = getPackageManager();
+            List<ApplicationInfo> listInfo = appInfo.getInstalledApplications(0);
+            Collections.sort(listInfo, new ApplicationInfo.DisplayNameComparator(appInfo));
+
+            List<PackageItem> data = new ArrayList<PackageItem>();
+
+            for (int index = 0; index < listInfo.size(); index++) {
+                try {
+                    ApplicationInfo content = listInfo.get(index);
+                    if ((content.flags != ApplicationInfo.FLAG_SYSTEM) && content.enabled) {
+                        if (content.icon != 0) {
+                            PackageItem item = new PackageItem();
+                            item.setName(getPackageManager().getApplicationLabel(content).toString());
+                            item.setPackageName(content.packageName);
+                            item.setIcon(getPackageManager().getDrawable(content.packageName, content.icon, content));
+                            data.add(item);
+                        }
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+
+            return data;
+        }
+
+        protected void onPostExecute(List<PackageItem> result) {
+            data.clear();
+            data.addAll(result);
+            adapter.notifyDataSetChanged();
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+                progressDialog = null;
+            }
+        }
+    }
+
+
+}
